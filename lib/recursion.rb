6a102874b7fa279e72d0d7929be1ec3c67dfad9a
Pedro <nebewu@gmail.com>
2022-04-03 15:34:01 -0300

    Implements iterative and recursive fibonacci

---
+++ lib/recursion.rb
@@ -0,0 +1,24 @@
+def fibs(n)
+  a = 0
+  b = 1
+  result = []
+
+  n.times do |entry|
+    result << a if entry == 0
+    result << b if entry == 1
+
+    if entry >= 2
+      result << (a + b)
+      a = b
+      b = result[-1]
+    end
+  end
+
+  result
+end
+
+def fibs_rec(n)
+  return [0] if n == 1
+  return [0,1] if n == 2
+  fibs_rec(n-1) + [fibs_rec(n-1)[-1] + fibs_rec(n-1)[-2]] if n >= 3
+end
